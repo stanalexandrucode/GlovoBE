b52b3a146ae0406799cc9f7f0de5f9ea13a4fbe8
stanalexandru <stanalexandrucode@gmail.com>
2021-07-04 17:10:17 +0300

    customized Http403ForbiddenEntryPoint sent exception

---
+++ src/main/java/com/cc/glovobe/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.cc.glovobe.filter;
+
+import com.cc.glovobe.Model.HttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.cc.glovobe.constant.SecurityConstant.FORBIDDEN_MESSAGE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+//it triggers when authentication fails and sends a response to the user(is like a handler when authentication fails)
+//Http403ForbiddenEntryPoint this will fire whenever the user fails to provide the authentication and they try to access the app
+//we customise what is sent to the user
+@Component
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(), FORBIDDEN_MESSAGE);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
