64a9d351c251c564f1b4b9c8ea757903a41024c6
stanalexandru <stanalexandrucode@gmail.com>
2021-07-04 14:24:25 +0300

    Implemented ExceptionHandling and custom exception response

---
+++ src/main/java/com/cc/glovobe/Controller/UserController.java
@@ -0,0 +1,10 @@
+package com.cc.glovobe.Controller;
+
+import com.cc.glovobe.exception.ExceptionHandling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = { "/", "/user"})
+public class UserController extends ExceptionHandling {
+}
