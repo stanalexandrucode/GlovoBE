d8d8e41cefeefe4e18b59fa66f6646e89d9a1f5e
stanalexandru <stanalexandrucode@gmail.com>
2021-07-04 14:31:50 +0300

    Implemented Role and Authority

---
+++ src/main/java/com/cc/glovobe/enumeration/Role.java
@@ -0,0 +1,23 @@
+package com.cc.glovobe.enumeration;
+
+import com.cc.glovobe.constant.Authority;
+
+import static com.cc.glovobe.constant.Authority.*;
+
+public enum Role {
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+}
