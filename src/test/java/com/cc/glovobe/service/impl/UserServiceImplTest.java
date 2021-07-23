8643170af582fd7f2f507b15f775abe620a358d0
Alex <stanalexandrucode@gmail.com>
2021-07-23 21:17:58 +0300

    Test test test

--- src/test/java/com/cc/glovobe/service/impl/UserServiceImplTest.java
+++ src/test/java/com/cc/glovobe/service/impl/UserServiceImplTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,38 +34,36 @@ import static com.cc.glovobe.constant.UserImplConstant.*;
 import static com.cc.glovobe.enumeration.Role.ROLE_USER;
 import static com.cc.glovobe.prototype.UsersPrototype.aUser;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
-    private UserServiceImpl underTest;
 
     @Mock
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
     @Mock
     private UserRepository userRepository;
-
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
-
     @Mock
     private ConfirmationTokenService confirmationTokenService;
-
     @Mock
     private EmailService emailService;
-
     @Mock
     private LoginAttemptService loginAttemptService;
-
     @Mock
     private TokenGenerator generateToken;
 
+
+    @InjectMocks
+    private UserServiceImpl underTest;
+
     @BeforeEach
-    void setUp() {
-        underTest = new UserServiceImpl(userRepository, confirmationTokenService, bCryptPasswordEncoder, emailService, loginAttemptService, generateToken);
+    public void setup() {
     }
 
     @Test
@@ -85,8 +84,7 @@ class UserServiceImplTest {
         userUnderTest.setAuthorities(ROLE_USER.getAuthorities());
         userUnderTest.setIsNonLocked(true);
         userUnderTest.setEnabled(false);
-
-        when(userRepository.findUserByEmail(usernameEmail)).thenReturn(userUnderTest);
+        when(userRepository.findUserByEmail(eq(usernameEmail))).thenReturn(userUnderTest);
 //        when
         UserDetails userDetails = underTest.loadUserByUsername(usernameEmail);
         //given
@@ -95,17 +93,20 @@ class UserServiceImplTest {
 
 
     @Test
-    void checkWhenEmailIsWrong() {
+    void checkLoadUserByUsernameWhenEmailIsWrong() {
+//        given
         String wrongEmail = "wrongEmail";
-        exceptionRule.expect(UsernameNotFoundException.class);
-        exceptionRule.expectMessage(USER_NOT_FOUND_WITH_EMAIL + wrongEmail);
         when(userRepository.findUserByEmail(wrongEmail)).thenReturn(null);
-        underTest.loadUserByUsername(wrongEmail);
+//        when
+//        then
+        assertThatThrownBy(() -> underTest.loadUserByUsername(wrongEmail))
+                .hasMessage(USER_NOT_FOUND_WITH_EMAIL + wrongEmail)
+                .isInstanceOf(UsernameNotFoundException.class);
     }
 
 
     @Test
-    void canRegister() throws EmailExistException, MessagingException {
+    void checkWhenRegister() throws EmailExistException, MessagingException {
 //        given
         when(generateToken.generateToken()).thenReturn("token123");
         RegistrationRequest request = new RegistrationRequest(
@@ -123,8 +124,7 @@ class UserServiceImplTest {
         user.setIsNonLocked(true);
         user.setEnabled(false);
 
-
-//        when
+//      when
         String tok = underTest.register(request);
 
 //      given
@@ -137,8 +137,8 @@ class UserServiceImplTest {
 
     @Test
     void checkWhenTokenIsWrongCanRegister() throws EmailExistException, MessagingException {
-        String token = "asdasdasdasda";
-        String wrongToken = "qwert";
+//        given
+        when(generateToken.generateToken()).thenReturn("token123");
         RegistrationRequest request = new RegistrationRequest(
                 "Alex",
                 "Stan",
@@ -154,55 +154,54 @@ class UserServiceImplTest {
         user.setIsNonLocked(true);
         user.setEnabled(false);
 
-//        when(underTest.createRegistrationToken(user)).thenReturn(token);
-//        underTest.createRegistrationToken(user);
-
-        when(underTest.register(request)).thenReturn(token);
+//      when
         String tok = underTest.register(request);
 
-        assertThat(tok).isNotEqualTo(wrongToken);
+//      given
+        verify(userRepository).save(user);
+        verify(emailService).sendAuthenticationLinkConfirmation(eq("token123"), eq(request.getEmail()));
+        assertThat(tok).isNotEqualTo("wrongEmail");
     }
 
     @Test
-    void willThrowWhenEmailExistInRegistration() throws EmailExistException, MessagingException {
+    void checkRegistrationWillThrowWhenEmailExist() throws EmailExistException {
 //        given
         RegistrationRequest request = new RegistrationRequest(
                 "Alex",
                 "Stan",
                 "nelu@gmail.com",
                 "password");
-
-        exceptionRule.expect(EmailExistException.class);
-        exceptionRule.expectMessage(EMAIL_ALREADY_EXIST + request.getEmail());
-        when(underTest.validateNewEmail(request.getEmail())).thenThrow(new EmailExistException(EMAIL_ALREADY_EXIST + request.getEmail()));
-        underTest.register(request);
-
+        given(underTest.validateNewEmail(eq("nelu@gmail.com"))).willAnswer(invocation -> {
+            throw new EmailExistException(EMAIL_ALREADY_EXIST + request.getEmail());
+        });
+//        when
+//        then
+        assertThatThrownBy(() -> underTest.register(request))
+                .hasMessage(EMAIL_ALREADY_EXIST + request.getEmail())
+                .isInstanceOf(EmailExistException.class);
 
     }
 
+//    @Test
+//    void checkRegistrationWillThrowWhenMessagingException() throws MessagingException {
+////        given
+//        String token = "asdsad";
+//        RegistrationRequest request = new RegistrationRequest(
+//                "Alex",
+//                "Stan",
+//                "nelu@gmail.com",
+//                "password");
+//    doThrow(new MessagingException("Error occurred")).when(emailService).sendAuthenticationLinkConfirmation(token, request.getEmail());
+//        emailService.sendAuthenticationLinkConfirmation(token, request.getEmail());
+////        when
+////        then
+//        assertThatThrownBy(() -> underTest.register(request))
+//                .isInstanceOf(MessagingException.class);
+//    }
 
     @Test
     void canConfirmToken() throws TokenNotFoundException, EmailExistException, TokenExpiredException {
-        String token = "asdasdasdasda";
-//        String msg = "registration completed"
-//        ConfirmationToken confirmationTokenBuild = new ConfirmationToken(
-//                token,
-//                LocalDateTime.now(),
-//                LocalDateTime.now().minus(Period.ofDays(10)),
-//                UsersPrototype.aUser()
-//
-//        );
-//
-//        when(confirmationTokenService.getToken(token)).thenReturn(confirmationTokenBuild);
-//        ConfirmationToken confirmationToken = confirmationTokenService.getToken(token);
-//        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
-//
-//        when(confirmationTokenService.setConfirmedAt(token)).thenReturn(confirmationTokenBuild);
-//
-//
-//        when(underTest.confirmToken(token)).thenReturn(msg);
-//        underTest.confirmToken(token);
-        underTest.confirmToken(token);
+
     }
 
 
